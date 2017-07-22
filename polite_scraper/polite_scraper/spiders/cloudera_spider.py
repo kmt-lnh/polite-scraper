c2ff51fdb77f6f600de3a6e1c3e7f7e3bc18789e
kmt-lnh <kmt-lnh@users.noreply.github.com>
2017-07-22 22:07:26 +0200

    initial commit
    with one example spider

---
+++ polite_scraper/polite_scraper/spiders/cloudera_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class ClouderaJobs(scrapy.Spider):
+    name = "cloudera"
+
+    def start_requests(self):
+        urls = [
+            'https://jobs.jobvite.com/cloudera/search?l=Hungary+%3E+Budapest&c=&q='
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for href in response.css("td.jv-job-list-name a::attr(href)").extract(): # and this extract the links
+            yield response.follow(href,self.parse_job)
+
+    def parse_job(self,response):
+        yield {
+            'jobtitle' : response.xpath("//h2/text()").extract_first().strip(),
+            'description' : response.xpath('//div[contains(@class, "jv-job-detail-description")]').extract()[0]
+        }
