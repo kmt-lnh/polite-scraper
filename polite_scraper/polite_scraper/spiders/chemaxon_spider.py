41d9938be20e36c524f272bd272c7aa600b437a9
kmt-lnh <kmt-lnh@users.noreply.github.com>
2017-08-08 23:18:05 +0200

    chemaxon spider

---
+++ polite_scraper/polite_scraper/spiders/chemaxon_spider.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class ChemaxonJobs(scrapy.Spider):
+    name = "chemaxon"
+
+    def start_requests(self):
+        urls = [
+            'https://www.chemaxon.com/jobs/'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    
+    def parse(self, response):
+        for href in response.css("div.masonry-item a::attr(href)").extract(): # and this extract the links
+            yield response.follow(href,self.parse_job)
+
+    def parse_job(self,response):
+        yield {
+            'jobtitle' : response.xpath("//h1/text()").extract_first().strip(),
+            'description' : response.xpath('//div[contains(@class,"entry")]').extract()[0]
+        }
+
