4ed9fcb61c281f13b46c157195c2f83c455465c1
kmt-lnh <kmt-lnh@users.noreply.github.com>
2017-08-10 22:25:56 +0200

    using xpath selectors instead of css

--- polite_scraper/polite_scraper/spiders/chemaxon_spider.py
+++ polite_scraper/polite_scraper/spiders/chemaxon_spider.py
@@ -6,14 +6,14 @@ class ChemaxonJobs(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://www.chemaxon.com/jobs/'
+            "https://www.chemaxon.com/jobs/"
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     
     def parse(self, response):
-        for href in response.css("div.masonry-item a::attr(href)").extract(): # and this extract the links
+        for href in response.xpath("//div[contains(@class, 'masonry-item')]/a/@href").extract(): # and this extract the links
             yield response.follow(href,self.parse_job)
 
     def parse_job(self,response):
--- polite_scraper/polite_scraper/spiders/cloudera_spider.py
+++ polite_scraper/polite_scraper/spiders/cloudera_spider.py
@@ -6,13 +6,13 @@ class ClouderaJobs(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://jobs.jobvite.com/cloudera/search?l=Hungary+%3E+Budapest&c=&q='
+            "https://jobs.jobvite.com/cloudera/search?l=Hungary+%3E+Budapest&c=&q="
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for href in response.css("td.jv-job-list-name a::attr(href)").extract(): # and this extract the links
+        for href in response.xpath("//td[contains(@class,'jv-job-list-name')]/a/@href").extract(): # and this extract the links
             yield response.follow(href,self.parse_job)
 
     def parse_job(self,response):
